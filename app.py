ad13e25f9c2cfeb4fd21665c0989f96f7d6a963c
nessou <inesouladjel@gmail.com>
2026-03-31 10:08:16 +0200

    add deploy.sh script

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
-print("Hello DevOps")
-print("Version 2")
+from flask import Flask
+app = Flask(__name__)
 
+@app.route('/')
+def hello():
+    return "Hello DevOps"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
